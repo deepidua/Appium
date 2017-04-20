7d29a4cdedf5598cc7fd96db99d0758d7b10072a
deepidua <deepikadua876@gmail.com>
2017-04-20 12:16:20 +0530

    Change-Id: I93c95b6dd0f73a7283bdbcd5b15c5d16857225c2

---
+++ Sel-Appium-Version1/src/one/Calci_Emulator.java
@@ -0,0 +1,63 @@
+
+/**
+ * Before running , launch the AVD in eclipse
+ * Launching Calculator using AVD and performing basic operation.
+ * 
+ */
+package one;
+
+import io.appium.java_client.android.AndroidDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class Calci_Emulator {
+
+	WebDriver driver;
+	
+	@Before
+	public void setUp() throws MalformedURLException 
+	{
+		DesiredCapabilities capabilities = DesiredCapabilities.android();
+		
+		capabilities.setCapability("BROWSER_NAME","Android");
+		capabilities.setCapability("deviceName","AVD_for_Redmi_Prime");
+		capabilities.setCapability("PLATFORM","Android");
+		capabilities.setCapability("VERSION","6.0.0");
+		capabilities.setCapability("appPackage","com.android.calculator2");
+		capabilities.setCapability("appActivity","com.android.calculator2.Calculator");
+		
+		driver = new AndroidDriver( new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+		
+		driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
+	}
+	@Test
+	public void testcal(){
+		
+		driver.findElement(By.xpath("//*[@text='2']")).click();
+		driver.findElement(By.xpath("//*[@text='+']")).click();
+		driver.findElement(By.xpath("//*[@text='7']")).click();
+		driver.findElement(By.xpath("//*[@text='=']")).click();
+		WebElement rs= driver.findElement(By.className("android.widget.EditText"));
+		System.out.println("results are "+rs);
+		
+	}
+	
+	@After
+	public void teardown(){
+		
+		driver.quit();
+	}
+	
+	
+	
+}
---
+++ Sel-Appium-Version1/src/one/Whatsapp_launch.java
@@ -0,0 +1,53 @@
+/**
+ * Launching Whatsapp application from appium.
+ * 
+ */
+package one;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+
+public class Whatsapp_launch {
+
+	WebDriver dr;
+
+	@Test
+	public void testApp() throws MalformedURLException, InterruptedException {
+
+		File app = new File(System.getProperty("user.dir")
+				+ "\\apks\\com.whatsapp_2.17.138-45.apk");
+
+		DesiredCapabilities capabilities = new DesiredCapabilities().android();
+		capabilities.setCapability("deviceName", "Redmi 3s");
+		capabilities.setCapability("platformVersion", "6.0.1");
+		capabilities.setCapability("platformName", "Android");
+		capabilities.setCapability("app", app.getAbsolutePath());
+		capabilities.setCapability("appPackage", "com.whatsapp");
+		capabilities.setCapability("appActivity", "com.whatsapp.Main");
+		Thread.sleep(1000);
+		dr = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),
+				capabilities);
+
+		dr.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
+
+		Thread.sleep(10000);
+
+	}
+
+	@After
+	public void quit() {
+		dr.quit();
+
+	}
+
+}
